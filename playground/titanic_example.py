c338a92fbd32fa1254c8dd093116568c93e6a2ea
Diomedes Git <diomedesgit@gmail.com>
2024-01-09 00:15:08 +0000

    describe

--- playground/titanic_example.py
+++ playground/titanic_example.py
@@ -1,6 +1,6 @@
-import pandas as pd
+import pandas as pd  # pd is the agreed abbreviation, may as well use it
 
-df = pd.DataFrame(
+df = pd.DataFrame(     # creating a pandas table
     {
         "Name": [
             "Braund, Mr. Owen Harris",
@@ -14,13 +14,19 @@ df = pd.DataFrame(
         
 )
 
-print(df)
+print(df)  # printing the table out 
+print(df["Age"]) # printing out the named column, ie the "Age" series in pandas temrinology 
 
-print(df["Age"])
+ages = df["Age"] # assigning series as a variable
+print(ages) 
 
-ages = df["Age"]
+ages = pd.Series([31, 44, 69], name="Age") # setting up a new series
 print(ages)
 
 
-ages = pd.Series([31, 44, 69], name="Age")
-print(ages)
+print(max(df["Age"]))    # equiv, all give max age,
+print(max(ages))         # but the 'ages' variable is reassigned above
+print(ages.max())        # so only the first one is accessing the original table
+
+print(df.describe())     # describe, a numerical method, so ignores textual
+
