462f85c0c103a9420fd9bf8847de9e37df48c6dc
Diomedes Git <diomedesgit@gmail.com>
2024-01-09 17:45:50 +0000

    more pandas, subframes from frames

--- playground/titanic_03.py
+++ playground/titanic_03.py
@@ -1,25 +1,27 @@
 import pandas as pd
 
-titanic = pd.read_csv('data/titanic.csv')
+titanic = pd.read_csv('data/titanic.csv')       # grabbing data, making into pd dataframe
 
-# print(titanic.head())
+# print(titanic.head())                         # checking intake of file 
 
-ages = titanic["Age"]
-print(ages.head(), '\n')
-print(titanic["Age"].shape, '\n')
-print(type(titanic["Age"]), '\n')
+ages = titanic["Age"]                           # declaring variable for series
+# print(ages.head(), '\n')                      # checking vairable via print of head/first five lines
+# print(titanic["Age"].shape, '\n')             # getting shape of 'Age' series
+# print(type(titanic["Age"]), '\n')             # printing series type check 
 
-fares = titanic["Fare"]
-print(fares.head(), '\n')
-print(type(titanic["Fare"]), '\n')
+fares = titanic["Fare"]                         # declaring variable for series
+# print(fares.head(), '\n')                     # checking vairable via print of head/first five lines
+# print(type(titanic["Fare"]), '\n')            # printing series type check 
 
-age_sex = titanic[["Age", "Sex"]]
-print(age_sex.head())
+age_sex = titanic[["Age", "Sex"]]               # declaring new variable, a dataframe containing two series from larger table 
+# print(age_sex.head())
 
-age_sex_fare = titanic[["Age", "Sex", "Fare"]]
-print(age_sex_fare.tail())
+age_sex_fare = titanic[["Age", "Sex", "Fare"]]  # declaring new variable, a dataframe containing three series from larger table 
+print(age_sex_fare)                             # checking variable via print
+print(type(age_sex_fare))                       # printing dataframe type check 
 
-print(max(ages))
+# print(max(ages))
+
 
 
 
