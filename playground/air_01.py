c7c9b16999d3636a10ec7012e87b71f95a88d684
Diomedes Git <diomedesgit@gmail.com>
2024-01-10 15:28:44 +0000

    air quality

--- playground/air_01.py
+++ playground/air_01.py
@@ -6,10 +6,24 @@ air_quality = pd.read_csv(
     index_col=0,                                  # and making 'datetime' into the index
     parse_dates=True)                             # and parsing the dates as 'Timestamp' objects 
 
-print(air_quality)
-air_quality.plot()                          # default plot creates one line per numeric column
-plt.show()                                  # displays plot
+# print(air_quality)
+# air_quality.plot()                          # default plot creates one line per numeric column
+# plt.show()                                  # displays plot
 
-air_quality["station_paris"].plot()         # plots named column only 
-plt.show()
+# air_quality["station_paris"].plot()         # plots named column only (plot works on Series or DataFrame)
+# plt.show()
+
+# air_quality.plot.scatter(                   # scatter plot
+#     x="station_london",                     # x declared as london column
+#     y="station_paris",                      # y declared as paris column
+#     alpha=0.5)                              # alpha transparency
+# plt.show()
+
+print(
+    [
+    method_name
+    for method_name in dir(air_quality.plot)
+    if not method_name.startswith("_")
+    ]
+)
 
