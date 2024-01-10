34a90e9edc57db0390e19892f72724db31486639
Diomedes Git <diomedesgit@gmail.com>
2024-01-10 16:31:16 +0000

    air quality 2

--- playground/air_02.py
+++ playground/air_02.py
@@ -21,3 +21,34 @@ air_quality[
 # all values in the given column are multiplied by the value 1.882 at once, 
 # no need to use a loop to iterate each of the rows
 
+
+air_quality["ratio_paris_antwerp"] = (
+    air_quality["station_paris"] / air_quality["station_antwerp"]
+)
+
+# making a ratio, but obvs all basic operators work
+# pd.DataFrame.apply() can also be used for more formulae 
+# (see https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.apply.html)
+
+print(air_quality)
+
+air_quality_renamed = air_quality.rename(           # rename() can be used for both row labels and column labels
+    columns = {                                     # use dictionary with the keys of current names/the values of new names to update the corresponding
+        "station_antwerp": "BETR801",
+        "station_paris": "FR04014",
+        "station_london": "London Westminster",
+    }
+)
+
+print(air_quality_renamed)
+
+air_quality_renamed = air_quality_renamed.rename(columns=str.lower)  # same idea, but mapping function, ie all str lower case
+print(air_quality_renamed.head())
+
+
+
+
+# ___________stuff__________
+# create new column by assigning output to  DataFrame with a new column name in between the [].
+# operations are element-wise, no need to loop
+# use rename with dictionary or function to rename row labels or column names
