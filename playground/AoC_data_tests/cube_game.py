ec459938b904837239076beabc553ef6e4c003e9
Diomedes Git <diomedesgit@gmail.com>
2024-01-19 23:39:49 +0000

    thinking some more about pandas as aoc tool

--- playground/AoC_data_tests/cube_game.py
+++ playground/AoC_data_tests/cube_game.py
@@ -1,35 +1,35 @@
 import pandas as pd
 
-data=pd.read_table("raw_games.txt", sep=':', header=None, names=["game", "rounds"])
-data["game"].str.strip("Game ")
-data["round"] = data["rounds"].str.split(';') 
+# data=pd.read_table("raw_games.txt", sep=':', header=None, names=["game", "rounds"])
+# data["game"].str.strip("Game ")
+# data["round"] = data["rounds"].str.split(';') 
 
-# print(data)
+# # print(data)
 
-game_tables = {}
+# game_tables = {}
 
-for _, row in data.iterrows():
-    game_name = row["game"]
-    rounds = row["rounds"]
+# for _, row in data.iterrows():
+#     game_name = row["game"]
+#     rounds = row["rounds"]
     
-    game_table = pd.DataFrame(columns=["red", "green", "blue"])
+#     game_table = pd.DataFrame(columns=["red", "green", "blue"])
     
-    for round_str in rounds:
-        round_data = {}
-        for cube_count in round_str.split(', '):
-            colour, count = cube_count.split(' ')
-            print(count)
-            # round_data[colour] = int(count)
-        game_tables.append(round_data, ignore_index = True)
-    game_table = game_table.fillna(0).astype(int)
+#     # for round_str in rounds:
+#     #     round_data = {}
+#     #     for cube_count in round_str.split(', '):
+#     #         colour, count = cube_count.split(' ')
+#     #         print(count)
+#     #         # round_data[colour] = int(count)
+#     #     game_tables.append(round_data, ignore_index = True)
+#     # game_table = game_table.fillna(0).astype(int)
     
-    game_tables[game_name] = game_table
+#     game_tables[game_name] = game_table
 
-print(game_tables[1])
-print(data["Red"].value_counts())
+# print(game_tables)
+# print(data["Red"].value_counts())
         
         
-# this approach seems quite 'unpandas', if that is a term
+# this approach seems quite 'unpanda' (if that is a term)
 # feels like using loops is sorta undermining the whole 
 # point of using sich a powerful tool; thinking like a dev,
 # and falling back on well-worn tools
@@ -43,3 +43,33 @@ print(data["Red"].value_counts())
 # manipulation is pretty in the zone for pd
 
 # also lol, just noticed i'm appending a dictionary
+
+
+
+    # red = 12
+    # green = 13
+    # blue = 14
+    
+game_list=pd.read_table("raw_games.txt", sep=':', header=None, names=["game", "rounds"])
+game_list["game"] = game_list["game"].str.strip("Game ")
+# game_list["rounds"] = game_list["rounds"].str.split(';') 
+# game_list["round"] = game_list["rounds"].str.split(';')
+
+print(
+    game_list.to_string(
+    buf=None,
+    columns=None, 
+    col_space=None, 
+    header=True, 
+    index=False, 
+    na_rep="NaN", 
+    formatters=None, 
+    float_format=None, 
+    index_names=True, 
+    justify=None, 
+    max_rows=None, 
+    max_cols=None, 
+    show_dimensions=False, 
+    decimal=".", 
+    line_width=None)
+)
