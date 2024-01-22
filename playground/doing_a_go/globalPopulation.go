050f65a200cfcbb7f6c38c878a4e7fd310362f2d
Diomedes Git <diomedesgit@gmail.com>
2024-01-22 23:57:14 +0000

    adding time taken to remind me to use it more often

--- playground/doing_a_go/globalPopulation.go
+++ playground/doing_a_go/globalPopulation.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
 
+	before := time.Now()
 	people := 1280
 	childrenPerPair := 5
 	generations := 0
@@ -19,6 +21,14 @@ func main() {
 	}
 
 	fmt.Printf("Population reached 7 billion in approximately %d generations, or %d years \n", generations, generations*25)
+	after := time.Now()
+
+	difference := before.Sub(after)
+
+	fmt.Println(before)
+	fmt.Println(after)
+	fmt.Print(difference)
+	
 }
 
 func randomMortalityFactor() float64 {
